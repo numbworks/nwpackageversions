ec56e7edc6361df5018639cc3c16e733319e0825
Rubèn <numbworks@gmail.com>
2024-10-07 13:34:40 +0000

    #1 SessionTestCase, added.

--- tests/nwpackageversionstests.py
+++ tests/nwpackageversionstests.py
@@ -7,7 +7,7 @@ from typing import Optional
 
 # LOCAL MODULES
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
-from nwpackageversions import XMLItem, Release
+from nwpackageversions import XMLItem, Release, Session
 
 # SUPPORT METHODS
 # TEST CLASSES
@@ -131,6 +131,66 @@ class ReleaseTestCase(unittest.TestCase):
         # Assert
         self.assertEqual(actual_str, expected)
         self.assertEqual(actual_repr, expected)
+class SessionTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.package_name: str = "numpy"
+        self.most_recent_version: str = "2.1.2"
+        self.most_recent_date: datetime = datetime(2024, 10, 5, 18, 28, 18)
+        self.releases: list[Release] = [
+            Release(package_name="numpy", version="2.1.2", date=datetime.strptime("2024-10-05", "%Y-%m-%d")),
+            Release(package_name="numpy", version="2.1.1", date=datetime.strptime("2024-09-03", "%Y-%m-%d")),
+            Release(package_name="numpy", version="2.0.2", date=datetime.strptime("2024-08-26", "%Y-%m-%d")),
+            Release(package_name="numpy", version="2.1.0", date=datetime.strptime("2024-08-18", "%Y-%m-%d"))
+        ]
+        self.xml_items: list[XMLItem] = [
+            XMLItem(title="2.1.2", link="https://pypi.org/project/numpy/2.1.2/", description="Fundamental package for array computing in Python", author=None, pubdate=datetime.strptime("Sat, 05 Oct 2024 18:28:18 GMT", "%a, %d %b %Y %H:%M:%S %Z"), pubdate_str="Sat, 05 Oct 2024 18:28:18 GMT"),
+            XMLItem(title="2.1.1", link="https://pypi.org/project/numpy/2.1.1/", description="Fundamental package for array computing in Python", author=None, pubdate=datetime.strptime("Tue, 03 Sep 2024 15:01:06 GMT", "%a, %d %b %Y %H:%M:%S %Z"), pubdate_str="Tue, 03 Sep 2024 15:01:06 GMT"),
+            XMLItem(title="2.0.2", link="https://pypi.org/project/numpy/2.0.2/", description="Fundamental package for array computing in Python", author=None, pubdate=datetime.strptime("Mon, 26 Aug 2024 20:04:14 GMT", "%a, %d %b %Y %H:%M:%S %Z"), pubdate_str="Mon, 26 Aug 2024 20:04:14 GMT"),
+            XMLItem(title="2.1.0", link="https://pypi.org/project/numpy/2.1.0/", description="Fundamental package for array computing in Python", author=None, pubdate=datetime.strptime("Sun, 18 Aug 2024 21:39:07 GMT", "%a, %d %b %Y %H:%M:%S %Z"), pubdate_str="Sun, 18 Aug 2024 21:39:07 GMT")
+        ]
+    def test_session_shouldinitializeasexpected_wheninvoked(self):
+	
+		# Arrange
+        # Act
+        session : Session = Session(
+            package_name = self.package_name,
+            most_recent_version = self.most_recent_version,
+            most_recent_date = self.most_recent_date,
+            releases = self.releases,
+            xml_items = self.xml_items
+        )
+		
+		# Assert
+        self.assertEqual(session.package_name, self.package_name)
+        self.assertEqual(session.most_recent_version, self.most_recent_version)
+        self.assertEqual(session.most_recent_date, self.most_recent_date)
+        self.assertEqual(session.releases, self.releases)
+        self.assertEqual(session.xml_items, self.xml_items)
+    def test_session_shouldreturnexpectedstring_whenargumentsarenotnone(self):
+        
+		# Arrange
+        expected: str = (
+            "{ 'package_name': 'numpy', "
+            "'most_recent_version': '2.1.2', "
+            "'most_recent_date': '2024-10-05', "
+            "'releases': '4', "
+            "'xml_items': '4' }"
+        )		
+		
+        # Act
+        session : Session = Session(
+            package_name = self.package_name,
+            most_recent_version = self.most_recent_version,
+            most_recent_date = self.most_recent_date,
+            releases = self.releases,
+            xml_items = self.xml_items
+        )
+        actual : str = str(session)
+
+        # Assert
+        self.assertEqual(actual, expected)
 
 # Main
 if __name__ == "__main__":
